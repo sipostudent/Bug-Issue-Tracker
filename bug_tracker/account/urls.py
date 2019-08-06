0a491c710efe1a00318259c7e27a6c13c6f7fa9b
Sipo Charles <sipocharles@yahoo.co.uk>
2019-08-06 04:50:23 +0100

    updated files

--- bug_tracker/account/urls.py
+++ bug_tracker/account/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login', views.login, name='login'),
-    path('logout', views.logout, name='logout'),
-    path('profile', views.profile, name='profile'),
-    path('profile/edit', views.edit_profile, name='edit_profile'),
-    path('register', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('profile/', views.profile, name='profile'),
+    path('profile/edit/', views.edit_profile, name='edit_profile'),
+    path('register/', views.register, name='register'),
 ]
--- bug_tracker/donations/urls.py
+++ bug_tracker/donations/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('donations', views.Donations.as_view(), name='donations'),
-    path('charge', views.charge, name='charge')
+    path('', views.Donations.as_view(), name='donations'),
+    path('charge/', views.charge, name='charge')
 ]
