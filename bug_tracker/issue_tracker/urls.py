25b49a04f6c13e06549c0a2b7beda231c5e05137
Sipo Charles <sipocharles@yahoo.co.uk>
2019-08-06 05:38:44 +0100

    updated urls.py paths

--- bug_tracker/issue_tracker/urls.py
+++ bug_tracker/issue_tracker/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('details/<id>', views.details, name='details'),
-    path('details/<id>/edit', views.edit_details, name='edit_details'),
-    path('create-ticket', views.create_ticket, name='create_ticket'),
-    path('details/<id>/delete', views.delete_ticket, name='delete_ticket')
+    path('details/<id>/', views.details, name='details'),
+    path('details/<id>/edit/', views.edit_details, name='edit_details'),
+    path('create-ticket/', views.create_ticket, name='create_ticket'),
+    path('details/<id>/delete/', views.delete_ticket, name='delete_ticket')
 ]
