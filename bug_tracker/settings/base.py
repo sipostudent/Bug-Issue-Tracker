4c94c0401f0f559f234518d3c157d959ca4b3d06
Sipo Charles <sipocharles@yahoo.co.uk>
2019-08-06 01:59:54 +0100

    configured website for production

--- bug_tracker/settings/base.py
+++ bug_tracker/settings/base.py
@@ -13,14 +13,14 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^24o&+_crc=lt$aljtgasi*q)#0e3#*v72&azr2s()-#a_5i_s'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -78,16 +79,6 @@ WSGI_APPLICATION = 'wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'pompycvj',
-        'USER': 'pompycvj',
-        'PASSWORD': 'h2AMZvS3rL9Vm-cgYGBm412z5FMtTbvZ',
-        'HOST': 'manny.db.elephantsql.com',
-        'PORT': '5432'
-    }
-}
 
 
 # Password validation
@@ -125,10 +116,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'bug_tracker/static')]
-
-STRIPE_PUBLISHABLE_KEY = 'pk_test_d7Mu02caiDOI3QXGmuG3xqoD00bCgZA6Vo'
-STRIPE_SECRET_KEY = 'sk_test_ekYXEwR2USDBKvgiiyfXxnCH00lma9s3Zr'
